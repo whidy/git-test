c6917e36598acca4f186c3a65bc5bcc2d16061de
Whidy_Macbook <whidy@whidy.net>
2019-11-18 09:30:40 +0800

    fix template command

--- script/command/template.js
+++ script/command/template.js
@@ -1,3 +1,4 @@
-const fs = require('fs');
+"use strict";
+var fs = require("fs");
 
-fs.copyFileSync('../../src/test.txt', '../../src/copied.txt');
+fs.copyFileSync("src/test.txt", "src/copied.txt");
